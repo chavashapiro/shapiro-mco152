7713daa17d9263dc607433027b53046aaae8876d
unknown <student1@FB-W215-W8.academic.touro.edu>
2015-09-10 19:29:05 -0400

    Added another test

--- src/shapiro/physics/ProjectileTest.java
+++ src/shapiro/physics/ProjectileTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class ProjectileTest {
 	
 	@Test
-	public void testGetX() {
+	public void testGetY() {
 		Projectile p = new Projectile(31, 20, 2.7);
-		double x = p.getX();
+		double x = p.getY();
 		
 		Assert.assertEquals(27.81, x, 0.01);
 	}
