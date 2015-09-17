3b657feb5ba7c761c860faa8b726fa94e2540b68
unknown <student1@FB-W215-W8.academic.touro.edu>
2015-09-17 19:58:25 -0400

    Changed ScrabbleDictionary to use HashSet

--- src/shapiro/scrabble/ScrabbleDictionary.java
+++ src/shapiro/scrabble/ScrabbleDictionary.java
@@ -2,15 +2,15 @@ package shapiro.scrabble;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ScrabbleDictionary {
 
-	private ArrayList<String> englishWords;
+	private HashSet<String> englishWords;
 
 	public ScrabbleDictionary() throws FileNotFoundException {
-		englishWords = new ArrayList<String>();
+		englishWords = new HashSet<String>();
 		Scanner wordsFile = new Scanner(new File("US.dic"));
 		while (wordsFile.hasNext()) {
 			englishWords.add(wordsFile.next());
