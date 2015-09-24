ea1824f16e715ac1d7ce0014da28cfc1ebd92ce0
unknown <student1@FB-W215-W8.academic.touro.edu>
2015-09-24 18:02:50 -0400

    changes to morsecode

--- src/shapiro/morsecode/MorseCode.java
+++ src/shapiro/morsecode/MorseCode.java
@@ -1,24 +1,31 @@
 package shapiro.morsecode;
 
+import java.util.HashMap;
+
 public class MorseCode {
-	private char[] letters;
+	private String[] letters;
 	private String[] morseLetters;
+	private HashMap <String, String> morseCode;
 
 	public MorseCode() {
-		letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-				'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
+		letters = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+				"r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
 		morseLetters = new String[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
 				".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
 				"--..", " " };
+		for (int i = 0; i < letters.length; i++) {
+			morseCode.put(letters[i], morseLetters[i]);
+			morseCode.put(morseLetters[i], letters[i]);
+		}
 	}
 
 	public String encode(String message) {
 		// turn message to all lowercase so will be easy to compare to chars in
 		// array
 		message = message.toLowerCase();
-		StringBuffer code = new StringBuffer();
+		StringBuilder code = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
-			for (int j = 0; j < letters.length; j++) {
+			/*for (int j = 0; j < letters.length; j++) {
 				if (message.charAt(i) == letters[j]) {
 					if (i == message.length() - 1) {
 						code.append(morseLetters[j]);
@@ -26,13 +33,18 @@ public class MorseCode {
 						code.append(morseLetters[j] + " ");
 					}
 				}
-			}
+			}*/
+			String morseLetter = morseCode.get(message.charAt(i));
+			
+				code.append(morseLetter);
+				code.append(" ");
+			
 		}
-		return code.toString();
+		return code.toString().trim();
 	}
 
 	public String decode(String code) {
-		StringBuffer message = new StringBuffer();
+		StringBuilder message = new StringBuilder();
 
 		String[] codeWordsArray = code.split("   ");
 		for (int t = 0; t < codeWordsArray.length; t++) {
