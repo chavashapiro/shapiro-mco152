e4fee0cbb9fa25ad0188e9204106634f1861eb0a
unknown <student1@FB-W215-W8.academic.touro.edu>
2015-10-29 20:29:35 -0400

    change scanner to buffered reader

--- src/shapiro/anagrams/MostAnagrams.java
+++ src/shapiro/anagrams/MostAnagrams.java
@@ -1,42 +1,48 @@
 package shapiro.anagrams;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner;
 
 public class MostAnagrams {
 	public static void main(String[] args) {
 		HashSet<String> englishWords = new HashSet<String>();
 		HashMap<String, ArrayList<String>> anagramGroups = new HashMap<String, ArrayList<String>>();
 
+		BufferedReader wordsFile;
 		try {
-			Scanner wordsFile = new Scanner(new File("US.dic"));
-			while (wordsFile.hasNext()) {
-				englishWords.add(wordsFile.next());
+			wordsFile = new BufferedReader(new FileReader("US.dic"));
+			String word;
+			while ((word = wordsFile.readLine()) != null) {
+				englishWords.add(word);
 			}
 			wordsFile.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("File not found. Program cannot execute.");
-			System.exit(0);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		for (String word : englishWords) {
-			word = word.toLowerCase();
-			String[] letters = word.split("");
+		for (String thisWord : englishWords) {
+			thisWord = thisWord.toLowerCase();
+			String[] letters = thisWord.split("");
 			// sort the word into alphabetical order
 			Arrays.sort(letters);
 			String sortedWord = Arrays.toString(letters);
 			// if a group with that word doesn't exist, create one. if it does,
 			// add this word
 			if (anagramGroups.containsKey(sortedWord)) {
-				anagramGroups.get(sortedWord).add(word);
+				anagramGroups.get(sortedWord).add(thisWord);
 			} else {
 				ArrayList<String> anagramWords = new ArrayList<String>();
-				anagramWords.add(word);
+				anagramWords.add(thisWord);
 				anagramGroups.put(sortedWord, anagramWords);
 			}
 		}
