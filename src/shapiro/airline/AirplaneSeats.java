e2a1cf436fca84846b56c1fab75012647bb9ad19
unknown <chavs915@gmail.com>
2015-11-16 19:34:32 -0500

    fix test

--- src/shapiro/airline/AirplaneSeats.java
+++ src/shapiro/airline/AirplaneSeats.java
@@ -1,7 +1,6 @@
 package shapiro.airline;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * This class is part of an Airline Reservation system. It holds seats that are
@@ -10,9 +9,7 @@ import java.util.HashMap;
  */
 public class AirplaneSeats {
 
-	private HashMap<Integer, ArrayList<Integer>> seats;
-	private HashMap<Character, Integer> letters;
-	private HashMap<Integer, Character> numbers;
+	private char[][] seats;
 	private int numRows;
 	private int numColumns;
 
@@ -23,65 +20,15 @@ public class AirplaneSeats {
 	 *            the number of columns of seats on the plane.
 	 */
 	public AirplaneSeats(int rows, int columns) {
-		letters = new HashMap<Character, Integer>();
-		letters.put('A', 1);
-		letters.put('B', 2);
-		letters.put('C', 3);
-		letters.put('D', 4);
-		letters.put('E', 5);
-		letters.put('F', 6);
-		letters.put('G', 7);
-		letters.put('H', 8);
-		letters.put('I', 9);
-		letters.put('J', 10);
-		letters.put('K', 11);
-		letters.put('L', 12);
-		letters.put('M', 13);
-		letters.put('N', 14);
-		letters.put('O', 15);
-		letters.put('P', 16);
-		letters.put('Q', 17);
-		letters.put('R', 18);
-		letters.put('S', 19);
-		letters.put('T', 20);
-		letters.put('U', 21);
-		letters.put('V', 22);
-		letters.put('W', 23);
-		letters.put('X', 24);
-		letters.put('Y', 25);
-		letters.put('Z', 26);
-
-		numbers = new HashMap<Integer, Character>();
-		numbers.put(1, 'A');
-		numbers.put(2, 'B');
-		numbers.put(3, 'C');
-		numbers.put(4, 'D');
-		numbers.put(5, 'E');
-		numbers.put(6, 'F');
-		numbers.put(7, 'G');
-		numbers.put(8, 'H');
-		numbers.put(9, 'I');
-		numbers.put(10, 'J');
-		numbers.put(11, 'K');
-		numbers.put(12, 'L');
-		numbers.put(13, 'M');
-		numbers.put(14, 'N');
-		numbers.put(15, 'O');
-		numbers.put(16, 'P');
-		numbers.put(17, 'Q');
-		numbers.put(18, 'R');
-		numbers.put(19, 'S');
-		numbers.put(20, 'T');
-		numbers.put(21, 'U');
-		numbers.put(22, 'V');
-		numbers.put(23, 'W');
-		numbers.put(24, 'X');
-		numbers.put(25, 'Y');
-		numbers.put(26, 'Z');
-
-		seats = new HashMap<Integer, ArrayList<Integer>>();
+		seats = new char[rows][columns];
 		numRows = rows;
 		numColumns = columns;
+
+		for (int row = 0; row < numRows; row++) {
+			for (int column = 0; column < numColumns; column++) {
+				seats[row][column] = 'o';
+			}
+		}
 	}
 
 	/**
@@ -94,25 +41,17 @@ public class AirplaneSeats {
 	 *             if the seat is outside the columns and rows set in the
 	 *             constructor
 	 */
-	public void reserve(String seatName) throws AlreadyReservedException, SeatOutOfBoundsException {
+	public void reserve(String seatName) throws AlreadyReservedException,
+			SeatOutOfBoundsException {
+		int column = seatName.charAt(0) - 65;
+		int row = Integer.parseInt(seatName.substring(1)) - 1;
+		if (row < 0 || column < 0 || row >= numRows || column >= numColumns) {
+			throw new SeatOutOfBoundsException();
+		}
 		if (isReserved(seatName)) {
 			throw new AlreadyReservedException();
 		}
-		int column = letters.get(seatName.charAt(0));
-		int row = Character.getNumericValue(seatName.charAt(1));
-		if (row < 1 || column < 1 || row > numRows || column > numColumns) {
-			throw new SeatOutOfBoundsException();
-		}
-		if (seats.containsKey(row)) {
-			seats.get(row).add((column - 1), 1);
-		} else {
-			ArrayList<Integer> rowSeats = new ArrayList<Integer>(26);
-			for (int i = 0; i < 26; i++) {
-				rowSeats.add(i, null);
-			}
-			rowSeats.add((column - 1), 1);
-			seats.put(row, rowSeats);
-		}
+		seats[row][column] = '#';
 	}
 
 	/**
@@ -122,13 +61,9 @@ public class AirplaneSeats {
 	 * @return true if the seat has been reserved, otherwise false.
 	 */
 	public boolean isReserved(String seatName) {
-		int column = letters.get(seatName.charAt(0));
-		int row = Character.getNumericValue(seatName.charAt(1));
-		if (seats.containsKey(row)) {
-			return seats.get(row).get(column - 1) != null;
-		} else {
-			return false;
-		}
+		int column = seatName.charAt(0) - 65;
+		int row = Integer.parseInt(seatName.substring(1)) - 1;
+		return (seats[row][column] == '#');
 	}
 
 	/**
@@ -143,7 +78,8 @@ public class AirplaneSeats {
 	 *             if one of the seats is outside the columns and rows set in
 	 *             the constructor
 	 */
-	public void reserveAll(String... seatNames) throws AlreadyReservedException, SeatOutOfBoundsException {
+	public void reserveAll(String... seatNames)
+			throws AlreadyReservedException, SeatOutOfBoundsException {
 		for (String seatName : seatNames) {
 			reserve(seatName);
 		}
@@ -164,24 +100,15 @@ public class AirplaneSeats {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("  ");
-		for (int i = 1; i <= numColumns; i++) {
-			builder.append(numbers.get(i));
+		for (int i = 0; i < numColumns; i++) {
+			char letter = (char) (i + 65);
+			builder.append(letter);
 		}
 		builder.append("\n");
-		for (int i = 1; i <= numRows; i++) {
-			builder.append(i + " ");
-			if (seats.containsKey(i)) {
-				for (int integer = 0; integer < numColumns; integer++) {
-					if (seats.get(i).get(integer) == null) {
-						builder.append("o");
-					} else {
-						builder.append("#");
-					}
-				}
-			} else {
-				for (int j = 1; j <= numColumns; j++) {
-					builder.append("o");
-				}
+		for (int row = 0; row < numRows; row++) {
+			builder.append(row + 1 + " ");
+			for (int column = 0; column < numColumns; column++) {
+				builder.append(seats[row][column]);
 			}
 			builder.append("\n");
 		}
@@ -201,74 +128,70 @@ public class AirplaneSeats {
 	 * @throws SeatOutOfBoundsException
 	 * @throws AlreadyReservedException
 	 */
-	public ArrayList<String> reserveGroup(int numberOfSeatsTogether) throws NotEnoughSeatsException,
-			AlreadyReservedException, SeatOutOfBoundsException {
+	public ArrayList<String> reserveGroup(int numberOfSeatsTogether)
+			throws NotEnoughSeatsException, AlreadyReservedException,
+			SeatOutOfBoundsException {
 
 		if (numberOfSeatsTogether > numColumns) {
 			throw new NotEnoughSeatsException();
 		}
 		if (numberOfSeatsTogether == numColumns) {
-			for (int i = 1; i <= numRows; i++) {
-				if (!seats.containsKey(i)) {
-					ArrayList<String> array = new ArrayList<String>();
-					for (int j = 1; j <= numberOfSeatsTogether; j++) {
+			for (int row = 0; row < numRows; row++) {
+				if (seats[row][0] == 'o') {
+					ArrayList<String> groupSeats = new ArrayList<String>();
+					int reserveRow = row + 1;
+					for (int column = 0; column < numColumns; column++) {
 						StringBuilder builder = new StringBuilder();
-						builder.append(numbers.get(j));
-						builder.append(i);
-						String builderString = builder.toString();
-						reserve(builderString);
-						array.add(builderString);
+						builder.append((char) (column + 65));
+						builder.append(reserveRow);
+						String seat = builder.toString();
+						reserve(seat);
+						groupSeats.add(seat);
 					}
-					return array;
+					return groupSeats;
 				}
 			}
 			throw new NotEnoughSeatsException();
 		}
 		// no need for if statement because if the code reached this point,
 		// obviously numberOfSeatsTogether < numColumns
-		for (int i = 1; i <= numRows; i++) {
-			if (seats.containsKey(i)) {
-				for (int j = 0; j < numberOfSeatsTogether; j++) {
+		for (int row = 0; row < numRows; row++) {
+			for (int j = 0; j < numberOfSeatsTogether; j++) {
+				if (seats[row][j] == 'o') {
 					boolean canWork = true;
-					for (int k = 0; k < numberOfSeatsTogether; k++) {
-						Integer newTemp = seats.get(i).get(j + k);
-						if (newTemp != null) {
+					for (int i = 1; i < numberOfSeatsTogether; i++) {
+						if (seats[row][j] != seats[row][j + 1]) {
 							canWork = false;
 							break;
 						}
 					}
 					if (canWork) {
-						ArrayList<String> array = new ArrayList<String>();
-						for (int k = 1; k <= numberOfSeatsTogether; k++) {
+						ArrayList<String> groupSeats = new ArrayList<String>();
+						int reserveRow = row + 1;
+						for (int column = j; column < numberOfSeatsTogether; column++) {
 							StringBuilder builder = new StringBuilder();
-							builder.append(numbers.get(k));
-							builder.append(i);
-							String builderString = builder.toString();
-							reserve(builderString);
-							array.add(builderString);
+							builder.append((char) (column + 65));
+							builder.append(reserveRow);
+							String seat = builder.toString();
+							reserve(seat);
+							groupSeats.add(seat);
 						}
-						return array;
+						return groupSeats;
 					}
 				}
 			}
-			if (!(seats.containsKey(i))) {
-				// I didn't have time to finish this method
-			}
 		}
-
-		return null;
+		// if no seats can be reserved together, throw exception
+		throw new NotEnoughSeatsException();
 	}
 
 	/**
 	 * @return true if the plane is full, otherwise false.
 	 */
 	public boolean isPlaneFull() {
-		for (int i = 1; i <= numRows; i++) {
-			if (!seats.containsKey(i)) {
-				return false;
-			}
+		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numColumns; j++) {
-				if (seats.get(i).get(j) == null) {
+				if (seats[i][j] == 'o') {
 					return false;
 				}
 			}
--- src/shapiro/airline/AirplaneSeatsTest.java
+++ src/shapiro/airline/AirplaneSeatsTest.java
@@ -1,5 +1,7 @@
 package shapiro.airline;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,26 +13,30 @@ public class AirplaneSeatsTest {
 	 */
 	public void testToStringWithEmptyPlane() {
 		AirplaneSeats seats = new AirplaneSeats(3, 4);
-		Assert.assertEquals("  ABCD\n" + "1 oooo\n" + "2 oooo\n" + "3 oooo\n", seats.toString());
+		Assert.assertEquals("  ABCD\n" + "1 oooo\n" + "2 oooo\n" + "3 oooo\n",
+				seats.toString());
 	}
 
 	@Test
 	/**
 	 * Test the output of toString() on an full plane. Do not modify this method.
 	 */
-	public void testToStringWithFullPlane() throws AlreadyReservedException, SeatOutOfBoundsException {
+	public void testToStringWithFullPlane() throws AlreadyReservedException,
+			SeatOutOfBoundsException {
 		AirplaneSeats seats = new AirplaneSeats(3, 4);
 		seats.reserveAll("A1", "B1", "C1", "D1");
 		seats.reserveAll("A2", "B2", "C2", "D2");
 		seats.reserveAll("A3", "B3", "C3", "D3");
-		Assert.assertEquals("  ABCD\n" + "1 ####\n" + "2 ####\n" + "3 ####\n", seats.toString());
+		Assert.assertEquals("  ABCD\n" + "1 ####\n" + "2 ####\n" + "3 ####\n",
+				seats.toString());
 	}
 
 	@Test
 	/**
 	 * Tests that reserve() reserves a seat correctly.
 	 */
-	public void testReserve() throws AlreadyReservedException, SeatOutOfBoundsException {
+	public void testReserve() throws AlreadyReservedException,
+			SeatOutOfBoundsException {
 		AirplaneSeats seats = new AirplaneSeats(1, 1);
 		seats.reserve("A1");
 		Assert.assertTrue(seats.isReserved("A1"));
@@ -41,7 +47,8 @@ public class AirplaneSeatsTest {
 	 * Tests that reserve() will throw an AlreadyReservedException if you attempt to reserve
 	 * a seat twice. Do not modify this method.
 	 */
-	public void testReserveThrowsAlreadyReservedException() throws SeatOutOfBoundsException {
+	public void testReserveThrowsAlreadyReservedException()
+			throws SeatOutOfBoundsException {
 		AirplaneSeats seats = new AirplaneSeats(1, 1);
 		try {
 			seats.reserve("A1");
@@ -57,7 +64,8 @@ public class AirplaneSeatsTest {
 	 * Tests that reserve() will thrown a SeatOutOfBoundsException is you attempt to reserve
 	 * a seat that is outside the bounds of the plane. 
 	 */
-	public void testReserveThrowsSeatOutOfBoundsException() throws AlreadyReservedException {
+	public void testReserveThrowsSeatOutOfBoundsException()
+			throws AlreadyReservedException {
 		AirplaneSeats seats = new AirplaneSeats(1, 1);
 		try {
 			seats.reserve("B1");
@@ -80,7 +88,8 @@ public class AirplaneSeatsTest {
 	/**
 	 * Tests that isFullPlane() returns true if there are no empty seats on the plane. 
 	 */
-	public void testIsPlaneFullReturnsTrue() throws AlreadyReservedException, SeatOutOfBoundsException {
+	public void testIsPlaneFullReturnsTrue() throws AlreadyReservedException,
+			SeatOutOfBoundsException {
 		AirplaneSeats seats = new AirplaneSeats(2, 1);
 		seats.reserve("A1");
 		seats.reserve("A2");
@@ -91,8 +100,15 @@ public class AirplaneSeatsTest {
 	/**
 	 * Tests that reserveGroup() reserves the correct seats when called on an empty plane.
 	 */
-	public void testReserveGroupOnEmptyPlane() throws NotEnoughSeatsException {
-		Assert.fail("Test not implemented");
+	public void testReserveGroupOnEmptyPlane() throws NotEnoughSeatsException,
+			AlreadyReservedException, SeatOutOfBoundsException {
+		AirplaneSeats seats = new AirplaneSeats(3, 3);
+		ArrayList<String> groupSeats = seats.reserveGroup(3);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("A1");
+		expected.add("B1");
+		expected.add("C1");
+		Assert.assertEquals(expected, groupSeats);
 	}
 
 	@Test
@@ -101,8 +117,18 @@ public class AirplaneSeatsTest {
 	 * seats already reserved. For instance, on a 3,4 airplane whose "A1" is occupied, 
 	 * calling reserveGroup(4) should return a list of elements ["A2", "B2", "C2", "D2"]
 	 */
-	public void testReserveGroupOnPartiallyFilledPlane() throws NotEnoughSeatsException {
-		Assert.fail("Test not implemented");
+	public void testReserveGroupOnPartiallyFilledPlane()
+			throws NotEnoughSeatsException, AlreadyReservedException,
+			SeatOutOfBoundsException {
+		AirplaneSeats seats = new AirplaneSeats(3, 4);
+		seats.reserve("A1");
+		ArrayList<String> groupSeats = seats.reserveGroup(4);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("A2");
+		expected.add("B2");
+		expected.add("C2");
+		expected.add("D2");
+		Assert.assertEquals(expected, groupSeats);
 	}
 
 	@Test
@@ -110,8 +136,42 @@ public class AirplaneSeatsTest {
 	 * Tests that reserveGroup() throws NotEnoughSeatsException if there are not enough 
 	 * seats available together for the group.
 	 */
-	public void testReserveGroupThrowsNotEnoughSeatsException() {
-		Assert.fail("Test not implemented");
+	public void testReserveGroupThrowsNotEnoughSeatsException()
+			throws AlreadyReservedException, SeatOutOfBoundsException {
+		AirplaneSeats seats = new AirplaneSeats(3, 3);
+		try {
+			seats.reserveGroup(4);
+			Assert.fail("Test fails. reserveGroup() should've thrown NotEnoughSeatsException");
+		} catch (NotEnoughSeatsException exc) {
+			// this is expected.
+		}
+		seats.reserve("A1");
+		seats.reserve("A2");
+		seats.reserve("A3");
+		try {
+			seats.reserveGroup(3);
+			Assert.fail("Test fails. reserveGroup() should've thrown NotEnoughSeatsException");
+		} catch (NotEnoughSeatsException ex) {
+			// This is expected. pass the test
+		}
+		seats.reserve("B1");
+		seats.reserve("B2");
+		seats.reserve("B3");
+		try {
+			seats.reserveGroup(2);
+			Assert.fail("Test fails. reserveGroup() should've thrown NotEnoughSeatsException");
+		} catch (NotEnoughSeatsException ex) {
+			// This is expected. pass the test
+		}
 	}
 
+	@Test
+	public void testReserveLargeSeatNumber() throws AlreadyReservedException,
+			SeatOutOfBoundsException {
+		AirplaneSeats seats = new AirplaneSeats(100, 26);
+		seats.reserve("A100");
+		Assert.assertFalse(seats.isReserved("A1"));
+		Assert.assertFalse(seats.isReserved("A10"));
+		Assert.assertTrue(seats.isReserved("A100"));
+	}
 }
