c5c8b8095c89becfec6ebe2a799a483550cd19da
unknown <student1@FB-W215-W8.academic.touro.edu>
2015-12-31 20:07:44 -0500

    jetty server

---
+++ src/shapiro/twitter/TwitterJettyServer.java
@@ -0,0 +1,14 @@
+package shapiro.twitter;
+
+import org.eclipse.jetty.server.Server;
+
+public class TwitterJettyServer {
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		server.setHandler(new TwitterHandler());
+
+		server.start();
+		server.join();
+	}
+}
